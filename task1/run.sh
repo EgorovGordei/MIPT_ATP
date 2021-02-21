6064e2c3fb74a1fd568ce685e7ba1fc73c9bb357
EgorovGordei <gordeiegorov@gmail.com>
2021-02-21 21:36:22 +0300

    Task1.5 commit

--- task1/run.sh
+++ task1/run.sh
@@ -9,11 +9,13 @@ mkdir $backup
 
 # find all files and copy them into all_files
 nice_files=$(find $path -type f -name "*.$dot")
+tmp=123456
 for fn in $nice_files
 do
   #mkdir -p "$backup/$fn"
   #cp $fn "$backup/$fn"
-  cp $fn $backup
+  cp $fn "$backup/$tmp"
+  tmp=$(($tmp+1))
 done
 tar -zcf $targz $nice_files
 #rm tmp_12345678913579
