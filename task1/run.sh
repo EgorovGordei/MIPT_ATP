0fe406cb088e21eec703764f57403e85d5fc9665
EgorovGordei <gordeiegorov@gmail.com>
2021-02-21 17:13:31 +0300

    Task1.1 commit

--- task1/run.sh
+++ task1/run.sh
@@ -7,14 +7,16 @@ targz=$4
 # create directory to copy all files
 mkdir $backup
 
+#tar -c -f $targz
 # find all files and copy them into all_files
 for fn in $(find $path -type f -name "*.$dot")
 do
   cp $fn $backup
+  tar -r -f $targz $fn
 done
 
 # make .tar.gz
-tar -c -f $targz $backup
+#tar -c -f $targz $backup
 
 # end of program
 echo done
