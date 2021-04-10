5e77d8f79378101e8d931909b2a0c686261a234a
EgorovGordei <egorov.ga@phystech.edu>
2021-04-10 23:43:20 +0300

    Changed

--- task1/run.sh
+++ task1/run.sh
@@ -1,15 +1,15 @@
 # getting parametres
-path=$1
-path_len=${#path}
-dot=$2
-backup=$3
-targz=$4
+#path=$1
+#path_len=${#path}
+#dot=$2
+#backup=$3
+#targz=$4
 
 # create directory to copy all files
-mkdir -p $backup
+mkdir -p $3
 
 # find all files
-nice_files=$(find $path -type f -name "*.$dot")
+nice_files=$(find $1 -type f -name "*.$2")
 
 # copy files
 #tmp=123456
@@ -17,16 +17,13 @@ nice_files=$(find $path -type f -name "*.$dot")
 for fn in $nice_files
 do
   crr_dir=$(dirname $fn)
-  dir_without_start=${crr_dir:$path_len}
+  dir_without_start=${crr_dir:${#$1}}
   dir_without_start=${dir_without_start:1}
 
-  #echo "$backup/$dir_without_start"
-  #mkdir "$backup/$crr_dir"
-  mkdir -p "$backup/$dir_without_start"
-  #cp $fn "$backup/$crr_dir/"
-  cp $fn "$backup/$dir_without_start"
+  mkdir -p "$3/$dir_without_start"
+  cp $fn "$3/$dir_without_start"
 done
-tar -zcf $targz $nice_files
+tar -zcf $4 $nice_files
 
 # end of program
 echo "done"
