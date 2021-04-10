d1eae5cf2d317f9ad9eb8f2463f89de991945029
EgorovGordei <egorov.ga@phystech.edu>
2021-04-10 23:54:54 +0300

    Ready for review

--- task1/run.sh
+++ task1/run.sh
@@ -12,12 +12,12 @@ mkdir -p $3
 nice_files=$(find $1 -type f -name "*.$2")
 
 # copy files
-#tmp=123456
-#echo $nice_files
 for fn in $nice_files
 do
   crr_dir=$(dirname $fn)
-  dir_without_start=${crr_dir:${#$1}}
+  path=$1
+  path_len=${#path}
+  dir_without_start=${crr_dir:$path_len}
   dir_without_start=${dir_without_start:1}
 
   mkdir -p "$3/$dir_without_start"
